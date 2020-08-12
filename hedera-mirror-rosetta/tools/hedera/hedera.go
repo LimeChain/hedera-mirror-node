1f60889a3f8dbdb4c331d8c402782b4605bef1d0
failfmi <oscurocalma@gmail.com>
2020-08-12 11:21:26 +0300

    refactor(hedera-mirror-rosetta): remove unused method

--- hedera-mirror-rosetta/tools/hedera/hedera.go
+++ hedera-mirror-rosetta/tools/hedera/hedera.go
@@ -1,17 +1,8 @@
 package hedera
 
 import (
-	"github.com/hashgraph/hedera-mirror-node/hedera-mirror-rosetta/app/domain/types"
 	"github.com/hashgraph/hedera-sdk-go"
 )
-
-func ToHederaAccountId(account *types.Account) hedera.AccountID {
-	return hedera.AccountID{
-		Shard:   uint64(account.Shard),
-		Realm:   uint64(account.Realm),
-		Account: uint64(account.Number),
-	}
-}
 
 func ToHbarAmount(amount int64) hedera.Hbar {
 	return hedera.HbarFromTinybar(amount)
