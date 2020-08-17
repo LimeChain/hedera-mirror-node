061385cbf71f9c70bae9980f179310a997db1c65
Daniel Ivanov <daniel@limechain.tech>
2020-08-17 17:43:18 +0300

    test6

--- hedera-mirror-rosetta/run-validation.sh
+++ hedera-mirror-rosetta/run-validation.sh
@@ -6,13 +6,12 @@ git clone https://github.com/coinbase/rosetta-cli.git
 cd ./rosetta-cli || exit 1
 
 echo Running Rosetta Data API Validation \#1
-if ! go run main.go check:data --configuration-file=./hedera-mirror-rosetta/validation/validate-from-genesis.json; then
+if ! go run main.go check:data --configuration-file=./validation/validate-from-genesis.json; then
     echo Failed to Pass API Validation \#1
     exit 1
 fi
-
 echo Running Rosetta Data API Validation \#2
-if ! go run main.go check:data --configuration-file=./hedera-mirror-rosetta/validation/validate-from-block-10.json; then
+if ! go run main.go check:data --configuration-file=./validation/validate-from-block-10.json; then
     echo Failed to Pass API Validation \#2
     exit 1
 fi
