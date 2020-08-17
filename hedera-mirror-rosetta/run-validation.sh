6503125ff40d99b780f5470ae553d9bc325ed6bc
Daniel Ivanov <daniel@limechain.tech>
2020-08-17 17:30:38 +0300

    test 3

--- hedera-mirror-rosetta/run-validation.sh
+++ hedera-mirror-rosetta/run-validation.sh
@@ -8,13 +8,13 @@ ls -la
 cd ./rosetta-cli
 
 echo Running Rosetta Data API Validation \#1
-if ! go run main.go check:data --configuration-file=./../validation/validate-from-genesis.json; then
+if ! go run main.go check:data --configuration-file=./hedera-mirror-rosetta/validation/validate-from-genesis.json; then
     echo Failed to Pass API Validation \#1
     exit 1
 fi
 
 echo Running Rosetta Data API Validation \#2
-if ! go run main.go check:data --configuration-file=./../validation/validate-from-block-10.json; then
+if ! go run main.go check:data --configuration-file=./hedera-mirror-rosetta/validation/validate-from-block-10.json; then
     echo Failed to Pass API Validation \#2
     exit 1
 fi
