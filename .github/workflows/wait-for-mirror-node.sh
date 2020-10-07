9c69a1c4e5e0adee3bd9c321b9fdeef3cc85d9b0
failfmi <oscurocalma@gmail.com>
2020-10-07 15:04:13 +0300

    infra: remove unnecessary logs
    Signed-off-by: failfmi <oscurocalma@gmail.com>

--- .github/workflows/wait-for-mirror-node.sh
+++ .github/workflows/wait-for-mirror-node.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 counter=0
-while [  $counter -lt 100 ];
-  do sleep 10
+while [  $counter -lt 120 ];
+  do sleep 1
   response=$(curl -sL -w "%{http_code}" -d '{"metadata":{}}' -i "http://localhost:5700/network/list")
   http_code=$(tail -n1 <<< "$response")
   if [ "$http_code" = "200" ]
@@ -11,7 +11,7 @@ while [  $counter -lt 100 ];
     exit 0
   else
     echo Mirror Node has not started yet...
-    counter=$((counter + 10))
+    counter=$((counter + 1))
   fi
 done
 
