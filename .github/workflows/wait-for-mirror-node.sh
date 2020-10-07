b7ff952680d2945e22e2234f786b59971c83f288
failfmi <oscurocalma@gmail.com>
2020-10-07 14:56:17 +0300

    infra: bump sleep time to 10s
    Signed-off-by: failfmi <oscurocalma@gmail.com>

--- .github/workflows/wait-for-mirror-node.sh
+++ .github/workflows/wait-for-mirror-node.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 counter=0
-while [  $counter -lt 120 ];
-  do sleep 1
+while [  $counter -lt 100 ];
+  do sleep 10
   response=$(curl -sL -w "%{http_code}" -d '{"metadata":{}}' -i "http://localhost:5700/network/list")
   http_code=$(tail -n1 <<< "$response")
   if [ "$http_code" = "200" ]
@@ -11,7 +11,7 @@ while [  $counter -lt 120 ];
     exit 0
   else
     echo Mirror Node has not started yet...
-    counter=$((counter + 1))
+    counter=$((counter + 10))
   fi
 done
 
