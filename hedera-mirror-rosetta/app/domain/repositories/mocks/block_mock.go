3f2e2008970c7941031defc1f07483b3b5a1353f
Georgi Yazovaliyski <georgi.yazovaliiski@gmail.com>
2020-10-09 16:37:35 +0300

    Account Service Unit tests
    Signed-off-by: Georgi Yazovaliyski <georgi.yazovaliiski@gmail.com>

--- hedera-mirror-rosetta/app/domain/repositories/mocks/block_mock.go
+++ hedera-mirror-rosetta/app/domain/repositories/mocks/block_mock.go
@@ -16,11 +16,15 @@ func (m MockBlockRepository) FindByIndex(index int64) (*types.Block, *rTypes.Err
 }
 
 func (m MockBlockRepository) FindByHash(hash string) (*types.Block, *rTypes.Error) {
-	panic("implement me")
+	fmt.Printf("Finding by hash [%s]", hash)
+	args := m.Called()
+	return args.Get(0).(*types.Block), nil
 }
 
 func (m MockBlockRepository) FindByIdentifier(index int64, hash string) (*types.Block, *rTypes.Error) {
-	panic("implement me")
+	fmt.Println("Finding by identifier")
+	args := m.Called()
+	return args.Get(0).(*types.Block), nil
 }
 
 func (m MockBlockRepository) RetrieveGenesis() (*types.Block, *rTypes.Error) {
@@ -30,5 +34,5 @@ func (m MockBlockRepository) RetrieveGenesis() (*types.Block, *rTypes.Error) {
 func (m MockBlockRepository) RetrieveLatest() (*types.Block, *rTypes.Error) {
 	fmt.Println("Retrieving latest")
 	args := m.Called()
-	return args.Get(0).(*types.Block), nil
+	return args.Get(0).(*types.Block), args.Get(1).(*rTypes.Error)
 }
--- hedera-mirror-rosetta/app/services/account_service_test.go
+++ hedera-mirror-rosetta/app/services/account_service_test.go
@@ -9,6 +9,22 @@ import (
 	"testing"
 )
 
+var mockBlockRepository *mocks.MockBlockRepository
+var mockTransactionRepository *mocks.MockTransactionRepository
+var mockAccountRepo *mocks.MockAccountRepository
+var exampleAccountService *AccountAPIService
+var exampleCommons Commons
+var nilBlock *types.Block = nil
+var nilError *rTypes.Error = nil
+
+func setup() {
+	mockBlockRepository = &mocks.MockBlockRepository{}
+	mockTransactionRepository = &mocks.MockTransactionRepository{}
+	mockAccountRepo = &mocks.MockAccountRepository{}
+	exampleCommons = NewCommons(mockBlockRepository, mockTransactionRepository)
+	exampleAccountService = NewAccountAPIService(exampleCommons, mockAccountRepo)
+}
+
 func exampleBlock() *types.Block {
 	return &types.Block{
 		Index:               1,
@@ -26,26 +42,24 @@ func exampleAmount() *types.Amount {
 	}
 }
 
-func exampleRequest() *rTypes.AccountBalanceRequest {
+func exampleRequest(withBlockIdentifier bool) *rTypes.AccountBalanceRequest {
+	var blockIdentifier *rTypes.PartialBlockIdentifier = nil
+	if withBlockIdentifier {
+		index := int64(1)
+		hash := "0x123"
+		blockIdentifier = &rTypes.PartialBlockIdentifier{
+			Index: &index,
+			Hash:  &hash,
+		}
+	}
 	return &rTypes.AccountBalanceRequest{
 		AccountIdentifier: &rTypes.AccountIdentifier{Address: "0.0.1"},
-		BlockIdentifier:   nil,
+		BlockIdentifier:   blockIdentifier,
 	}
 }
 
-func TestAccountBalance(t *testing.T) {
-	// mocks:
-	mockBlockRepository := &mocks.MockBlockRepository{}
-	mockTransactionRepository := &mocks.MockTransactionRepository{}
-	mockAccountRepo := &mocks.MockAccountRepository{}
-
-	mockBlockRepository.On("RetrieveLatest").Return(exampleBlock())
-	mockAccountRepo.On("RetrieveBalanceAtBlock").Return(exampleAmount())
-
-	// given:
-	exampleCommons := NewCommons(mockBlockRepository, mockTransactionRepository)
-	exampleAccountService := NewAccountAPIService(exampleCommons, mockAccountRepo)
-	expectedResult := &rTypes.AccountBalanceResponse{
+func expectedAccountBalanceResponse() *rTypes.AccountBalanceResponse {
+	return &rTypes.AccountBalanceResponse{
 		BlockIdentifier: &rTypes.BlockIdentifier{
 			Index: 1,
 			Hash:  "0x123jsjs",
@@ -57,12 +71,47 @@ func TestAccountBalance(t *testing.T) {
 			},
 		},
 	}
-	var expectedNilError *rTypes.Error = nil
+}
+
+func TestAccountBalance(t *testing.T) {
+	// mocks:
+	setup()
+	mockBlockRepository.On("RetrieveLatest").Return(exampleBlock(), nilError)
+	mockAccountRepo.On("RetrieveBalanceAtBlock").Return(exampleAmount(), nilError)
 
 	// when:
-	actualResult, error := exampleAccountService.AccountBalance(nil, exampleRequest())
+	actualResult, e := exampleAccountService.AccountBalance(nil, exampleRequest(false))
 
 	// then:
-	assert.Equal(t, expectedResult, actualResult)
-	assert.Equal(t, expectedNilError, error)
+	assert.Equal(t, expectedAccountBalanceResponse(), actualResult)
+	assert.Nil(t, e)
+}
+
+func TestAccountBalanceFullData(t *testing.T) {
+	// mocks:
+	setup()
+	mockBlockRepository.On("FindByIdentifier").Return(exampleBlock(), nilError)
+	mockAccountRepo.On("RetrieveBalanceAtBlock").Return(exampleAmount(), nilError)
+
+	// when:
+	actualResult, e := exampleAccountService.AccountBalance(nil, exampleRequest(true))
+
+	//then:
+	assert.Equal(t, expectedAccountBalanceResponse(), actualResult)
+	assert.Nil(t, e)
+	mockBlockRepository.AssertNotCalled(t, "RetrieveLatest")
+}
+
+func TestAccountBalanceThrows(t *testing.T) {
+	// mocks:
+	setup()
+
+	mockBlockRepository.On("RetrieveLatest").Return(nilBlock, &rTypes.Error{})
+
+	// when:
+	actualResult, e := exampleAccountService.AccountBalance(nil, exampleRequest(false))
+
+	// then:
+	assert.Nil(t, actualResult)
+	assert.NotNil(t, e)
 }
